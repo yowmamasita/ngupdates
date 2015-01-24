92ac9617df982db89a5f9a75d4bddefd359861e9
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2015-01-24 17:55:06 +0800

    news fetcher

--- app/controllers/cron.py
+++ app/controllers/cron.py
@@ -1,5 +1,12 @@
 from ferris import Controller, messages, route_with
 from app.models.post import Post
+import requests
+import time
+
+
+API_HOSTNAME = "http://hn.algolia.com"
+API_VERSION = "v1"
+FREQUENCY = 86400  # fetches daily
 
 
 class Cron(Controller):
@@ -10,4 +17,9 @@ class Cron(Controller):
 
     @route_with('/api/cron/fetch_news')
     def api_fetch_news(self):
-        pass
+        ts = int(time.time()) - FREQUENCY
+        action = "search_by_date?query=angular&tags=story&numericFilters=created_at_i>%s" % ts
+        url = "%s/api/%s/%s" % (API_HOSTNAME, API_VERSION, action,)
+        r = requests.get(url)
+        data = r.json()
+        print data
